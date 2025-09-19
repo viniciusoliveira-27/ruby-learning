ee77b2ea9cbb3f7982bcf722b8b7d2605b02c9a6
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-09-19 16:23:02 -0300

    feat: add metaprogramacao learn

---
+++ metaprogramacao/modulo.rb
@@ -0,0 +1,60 @@
+module Atributos
+	def self.included(base)
+
+		base.class_eval do
+			def xxx
+				puts "sssss"
+			end
+		end
+
+		base.instance_eval do
+			def xxx_de_classe
+				puts "sssss"
+			end
+		end
+
+		puts "Foi incluido na classe - #{base}"
+	end
+
+	attr_accessor :id, :nome, :documento
+
+	def mostrar_dados
+		puts "#{id} - #{nome} - #{documento}"
+	end
+end
+
+module MetodosDeClasse
+	def metodo_total
+		puts "Oiiiii ... "
+	end
+end
+
+class Cliente
+	include Atributos
+	extend MetodosDeClasse
+end
+
+class Fornecedor
+	include Atributos
+	extend MetodosDeClasse
+end
+
+c = Cliente.new
+f = Fornecedor.new
+# c.id = 1
+# c.nome = "Danilo"
+# c.documento = "12222"
+
+# c.mostrar_dados
+puts "===== instancia cliente ====="
+puts c.methods - Class.methods
+puts "===== instancia fornecedor ====="
+puts f.methods - Class.methods
+
+puts "===== classe cliente ====="
+puts Cliente.methods - Class.methods
+puts "===== classe fornecedor ====="
+puts Fornecedor.methods - Class.methods
+
+
+# Cliente.metodo_total
